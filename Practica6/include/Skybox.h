22bdfcb905e7314864e05a23de873e89a1e53ac0
Axel N <AxelNunezArzola11@users.noreply.github.com>
2024-04-14 23:03:56 -0600

    Agregar archivos de la Práctica 6 ejecutables en carpeta build

---
+++ Practica6/include/Skybox.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "CommonValues.h"
+#include <vector>
+#include <string>
+/*
+pa windows
+#include<glew.h>
+#include<glm.hpp>
+#include<glfw3.h>
+#include <gtc\matrix_transform.hpp>
+#include<gtc\type_ptr.hpp>*/
+///pa linux
+#include <GL/glew.h>
+#include<glm/glm.hpp>
+#include <GLFW/glfw3.h>
+#include<glm/gtc/matrix_transform.hpp>
+#include<glm/gtc/type_ptr.hpp>
+#include "Mesh.h"
+#include "Shader_m.h"
+class Skybox
+{
+public:
+	Skybox();
+	Skybox(std::vector<std::string> faceLocations);
+	void DrawSkybox(glm::mat4 viewMatrix, glm::mat4 projectionMatrix);
+		
+	~Skybox();
+private:
+	Mesh* skyMesh;
+	Shader* skyShader;
+	GLuint textureId;
+	GLuint uniformProjection, uniformView;
+
+};
+
---
+++ Practica6/include/Window.h
@@ -0,0 +1,60 @@
+#pragma once
+#include<stdio.h>
+/* pa windows
+#include<glew.h>
+#include<glfw3.h>
+*/
+//linux
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+class Window
+{
+public:
+	Window();
+	Window(GLint windowWidth, GLint windowHeight);
+	int Initialise();
+	GLfloat getBufferWidth() { return bufferWidth; }
+	GLfloat getBufferHeight() { return bufferHeight; }
+	GLfloat getXChange();
+	GLfloat getYChange();
+	GLfloat getmuevex() { return muevex; }
+	GLfloat getangulocola() { return angulocola; }
+	bool getShouldClose() {
+		return  glfwWindowShouldClose(mainWindow);}
+	bool* getsKeys() { return keys; }
+	void swapBuffers() { return glfwSwapBuffers(mainWindow); }
+
+	GLfloat getangulorueda() { return angulorueda; }
+//cofre
+GLfloat getangulocofre() { return angulocofre; }
+///DESPLAZAMIENTO
+GLfloat getdesplazamiento(){ return desplazamiento;}
+
+	
+	~Window();
+private: 
+	GLFWwindow *mainWindow;
+	GLint width, height;
+	////rueda
+GLfloat angulorueda=0.0f;
+///cofre
+GLfloat angulocofre=0.0f;
+///desplazamiento
+GLfloat desplazamiento= 0.0f;
+
+	bool keys[1024];
+	GLint bufferWidth, bufferHeight;
+	void createCallbacks();
+	GLfloat lastX;
+	GLfloat lastY;
+	GLfloat xChange;
+	GLfloat yChange;
+	GLfloat muevex;
+	GLfloat angulocola=0.0f;
+	bool mouseFirstMoved;
+	static void ManejaTeclado(GLFWwindow* window, int key, int code, int action, int mode);
+	static void ManejaMouse(GLFWwindow* window, double xPos, double yPos);
+
+};
+
